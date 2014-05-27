31e55940bc7a6d40b2b2f1a32734560edcef442c
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-05-27 12:29:20 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guiav::Application.config.secret_key_base = 'fed6c8d022fc7f9eaa9366375564e3442bd368275ed3ca8b03161a2c4d4f1dd6145d205dfa5288456e90ed3385045651db7fa29740e160185246028292522755'
