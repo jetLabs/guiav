65baf755cb8316f6de439cd3fbc8555be209a1b9
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-06-03 21:21:23 -0300

    Hover of the icons links fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,14 @@ class ApplicationController < ActionController::Base
 
   helper_method :paypal_url
   #before_action :authorize
-  def paypal_url(return_url)
+  def paypal_url
 
     values = {
       # get it form your http://sandbox.paypal.com account
       :business => 'jandres.rodriguezg-facilitator@gmail.com',
       :cmd => '_cart',
       :upload => 1,
-      :return => return_url,
+      :return => "http://guiav.herokuapp.com/",
       :no_shipping => 1
     }
     # These values set up the details for the item on paypal.
